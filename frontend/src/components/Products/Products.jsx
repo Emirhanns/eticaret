222be58245546a5472892a4e7dc06c401b3b85e9
Emirhan Çetin <ctn.emrhn0@gmail.com>
2024-11-30 18:51:48 +0300

    Update Products.jsx

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
-import Categories from "../Categories/Categories";  // Categories burada sadece bir kez yer alacak
+import Categories from "../Categories/Categories";
+import { Modal, Button } from "antd"; // Modal ve Button'ı Ant Design'dan ekliyoruz
 import "./Products.css";
 import { message } from "antd";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null);  // Seçilen kategori
+  const [selectedCategory, setSelectedCategory] = useState(null); // Seçilen kategori
+  const [isModalVisible, setIsModalVisible] = useState(false); // Modal görünürlüğü
   const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
@@ -28,6 +30,15 @@ const Products = () => {
 
   const handleCategorySelect = (categoryId) => {
     setSelectedCategory(categoryId); // Kategori seçildiğinde filtreyi uygula
+    setIsModalVisible(false); // Modal'ı kapat
+  };
+
+  const handleShowModal = () => {
+    setIsModalVisible(true); // Modal'ı aç
+  };
+
+  const handleCancelModal = () => {
+    setIsModalVisible(false); // Modal'ı kapat
   };
 
   const filteredProducts = selectedCategory
@@ -37,8 +48,16 @@ const Products = () => {
   return (
     <section className="products">
       <div className="container">
-        <Categories onCategorySelect={handleCategorySelect} selectedCategory={selectedCategory} />  {/* Categories burada sadece bir kez yer alacak */}
+        
         <h2 className="section-title">Tüm Ürünler</h2>
+        <div className="category-menu-button">
+          <Button
+            type="primary"
+            onClick={handleShowModal}
+          >
+            Kategori Seç
+          </Button>
+        </div>
         <div className="product-wrapper">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((product) => (
@@ -49,6 +68,17 @@ const Products = () => {
           )}
         </div>
       </div>
+
+      {/* Modal Kategorileri Seçme */}
+      <Modal
+        title="Kategori Seç"
+        visible={isModalVisible}
+        onCancel={handleCancelModal}
+        footer={null} // Footer kısmını kaldırabiliriz
+        width={400} // Modal genişliğini ayarlayabiliriz
+      >
+        <Categories onCategorySelect={handleCategorySelect} selectedCategory={selectedCategory} />
+      </Modal>
     </section>
   );
 };
