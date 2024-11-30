9020981c6dbf820be5b83c86cdc099b5ccb73ee5
Emirhan Çetin <ctn.emrhn0@gmail.com>
2024-11-30 12:37:26 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin/', // veya uygulamanızın yoluna uygun bir değer
   plugins: [react()],
   build: {
     outDir: 'dist', // Çıktı klasörünü açıkça belirtin
