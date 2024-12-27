383b8ceceb7691cfa04e10a001dde9fb986cd344
Emirhan Çetin <ctn.emrhn0@gmail.com>
2024-12-27 15:04:34 +0300

    asdasdasdasdsad

--- frontend/src/components/ProductDetails/Gallery/Gallery.jsx
+++ frontend/src/components/ProductDetails/Gallery/Gallery.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Slider from "react-slick";
 import "./Gallery.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 function PrevBtn({ onClick }) {
   return (
     <button
       className="glide__arrow glide__arrow--left"
       onClick={onClick}
-      style={{ zIndex: "2" }}
+      style={{
+        zIndex: "2",
+      }}
     >
       <i className="bi bi-chevron-left"></i>
     </button>
@@ -22,23 +22,28 @@ function NextBtn({ onClick }) {
     <button
       className="glide__arrow glide__arrow--right"
       onClick={onClick}
-      style={{ zIndex: "2" }}
+      style={{
+        zIndex: "2",
+      }}
     >
       <i className="bi bi-chevron-right"></i>
     </button>
   );
 }
 
-PrevBtn.propTypes = {
-  onClick: PropTypes.func,
-};
-
 NextBtn.propTypes = {
   onClick: PropTypes.func,
 };
 
+PrevBtn.propTypes = {
+  onClick: PropTypes.func,
+};
+
 const Gallery = ({ singleProduct }) => {
-  const [activeImg, setActiveImg] = useState({ img: "", imgIndex: 0 });
+  const [activeImg, setActiveImg] = useState({
+    img: "",
+    imgIndex: 0,
+  });
 
   useEffect(() => {
     if (singleProduct.img && singleProduct.img.length > 0) {
@@ -47,11 +52,11 @@ const Gallery = ({ singleProduct }) => {
   }, [singleProduct.img]);
 
   const sliderSettings = {
-    dots: false,
-    infinite: singleProduct.img.length > 1,
-    slidesToShow: 1,
+    dots: true,
+    infinite: singleProduct.img.length > 1, // Tek görsel varsa sonsuz kaydırmayı devre dışı bırak
+    slidesToShow: 1, // Aynı anda sadece bir görsel göstermek için
     slidesToScroll: 1,
-    nextArrow: singleProduct.img.length > 1 ? <NextBtn /> : null,
+    nextArrow: singleProduct.img.length > 1 ? <NextBtn /> : null, // Tek görsel varsa butonları gizle
     prevArrow: singleProduct.img.length > 1 ? <PrevBtn /> : null,
   };
 
@@ -59,35 +64,25 @@ const Gallery = ({ singleProduct }) => {
     <div className="product-gallery">
       <div className="single-image-wrapper">
         <Slider {...sliderSettings}>
-          {singleProduct.img && singleProduct.img.length > 0 ? (
-            singleProduct.img.map((itemImg, index) => (
-              <div key={index} className="image-slide">
-                <img
-                  src={itemImg}
-                  alt={`Product ${index}`}
-                  className="img-fluid"
-                />
-              </div>
-            ))
-          ) : (
-            <div className="image-slide">
+          {singleProduct.img.map((itemImg, index) => (
+            <div key={index} className="image-slide">
               <img
-                src="https://via.placeholder.com/800x600?text=No+Image"
-                alt="Default"
+                src={itemImg}
+                alt={`Product ${index}`}
                 className="img-fluid"
               />
             </div>
-          )}
+          ))}
         </Slider>
       </div>
     </div>
   );
 };
 
+export default Gallery;
+
 Gallery.propTypes = {
   singleProduct: PropTypes.shape({
     img: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
 };
-
-export default Gallery;
