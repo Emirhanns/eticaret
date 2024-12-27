04dbe18fa3aceadb5c4273a12192b46481e83249
Emirhan Çetin <ctn.emrhn0@gmail.com>
2024-12-27 14:47:44 +0300

    asd

--- frontend/src/components/ProductDetails/Gallery/Gallery.jsx
+++ frontend/src/components/ProductDetails/Gallery/Gallery.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Slider from "react-slick";
 import "./Gallery.css";
-import { useEffect } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function PrevBtn({ onClick }) {
   return (
     <button
       className="glide__arrow glide__arrow--left"
-      data-glide-dir="<"
       onClick={onClick}
-      style={{
-        zIndex: "2",
-      }}
+      style={{ zIndex: "2" }}
     >
       <i className="bi bi-chevron-left"></i>
     </button>
@@ -23,87 +22,51 @@ function NextBtn({ onClick }) {
   return (
     <button
       className="glide__arrow glide__arrow--right"
-      data-glide-dir=">"
       onClick={onClick}
-      style={{
-        zIndex: "2",
-      }}
+      style={{ zIndex: "2" }}
     >
       <i className="bi bi-chevron-right"></i>
     </button>
   );
 }
 
-NextBtn.propTypes = {
-  onClick: PropTypes.func,
-};
-
 PrevBtn.propTypes = {
   onClick: PropTypes.func,
 };
 
-const Gallery = ({ singleProduct }) => {
-  const [activeImg, setActiveImg] = useState({
-    img: "",
-    imgIndex: 0,
-  });
-
-  useEffect(() => {
-    setActiveImg({ img: singleProduct.img[0], imgIndex: 0 });
-  }, [singleProduct.img]);
-
-const sliderSettings = {
-  dots: false,
-  infinite: singleProduct.img.length > 1, // Tek görsel varsa sonsuz kaydırmayı devre dışı bırak
-  slidesToShow: singleProduct.img.length > 1 ? Math.min(singleProduct.img.length, 4) : 1, // Görsel sayısına göre ayar
-  slidesToScroll: 1,
-  nextArrow: singleProduct.img.length > 1 ? <NextBtn /> : null, // Tek görsel varsa butonları gizle
-  prevArrow: singleProduct.img.length > 1 ? <PrevBtn /> : null,
+NextBtn.propTypes = {
+  onClick: PropTypes.func,
 };
 
-console.log(singleProduct.img);
-
+const Gallery = ({ singleProduct }) => {
+  const sliderSettings = {
+    dots: false,
+    infinite: singleProduct.img.length > 1,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: singleProduct.img.length > 1 ? <NextBtn /> : null,
+    prevArrow: singleProduct.img.length > 1 ? <PrevBtn /> : null,
+  };
 
   return (
     <div className="product-gallery">
       <div className="single-image-wrapper">
-        <img src={`${activeImg.img}`} id="single-image" alt="" />
-      </div>
-      <div className="product-thumb">
-        <div className="glide__track" data-glide-el="track">
-          <ol className="gallery-thumbs glide__slides">
-            <Slider {...sliderSettings}>
-              {singleProduct.img.map((itemImg, index) => (
-                <li
-                  className="glide__slide glide__slide--active"
-                  key={index}
-                  onClick={() =>
-                    setActiveImg({
-                      img: itemImg,
-                      imgIndex: index,
-                    })
-                  }
-                >
-                  <img
-                    src={`${itemImg}`}
-                    alt=""
-                    className={`img-fluid ${
-                      activeImg.imgIndex === index ? "active" : ""
-                    } `}
-                  />
-                </li>
-              ))}
-            </Slider>
-          </ol>
-        </div>
-        <div className="glide__arrows" data-glide-el="controls"></div>
+        <Slider {...sliderSettings}>
+          {singleProduct.img.map((itemImg, index) => (
+            <div key={index} className="image-slide">
+              <img src={itemImg} alt={`Product ${index}`} className="img-fluid" />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
 };
 
-export default Gallery;
-
 Gallery.propTypes = {
-  singleProduct: PropTypes.object,
+  singleProduct: PropTypes.shape({
+    img: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
 };
+
+export default Gallery;
